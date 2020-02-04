e94ef222065326b3df541b191ca62b6667cc76fb
jonny rhea <jonathan.rhea@gmail.com>
2020-02-04 13:18:28 -0600

    stubbed out engine and primitves

---
+++ src/engine/mod.rs
@@ -0,0 +1,27 @@
+
+pub struct Engine {
+
+}
+
+impl Engine {
+
+    pub fn new() -> Engine {
+        Engine{ }
+    }
+
+    /// Deploy EE code onchain
+    pub fn deploy(&self, code:Vec<u8>, state:Vec<u8>) {
+
+    }
+
+    /// Send tx to be run onchain
+    pub fn run(&self, tx:Vec<u8>) {
+
+    }
+
+    /// Get onchain state root
+    pub fn get_root(&self) {
+
+    }
+
+}
