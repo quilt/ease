e7f7dba9e7205ac8a802d10f66cce59b5e21bdd6
jonny rhea <jonathan.rhea@gmail.com>
2020-02-11 14:22:45 -0600

    adding dummy simulation implementation in order to work out engine methods

---
+++ src/engine/simulation.rs
@@ -0,0 +1,59 @@
+use std::cell::Cell;
+use std::mem::transmute;
+use ewasm::{Execute, RootRuntime};
+
+pub struct Execution_Environment {
+    code: Vec<u8>,
+    state: Vec<u8>,
+    root: Cell<[u8; 32]>
+}
+
+pub struct Simulation
+{
+    execution_environments: Vec<Execution_Environment>,
+}
+
+impl Simulation {
+    
+    pub fn new() -> Simulation {
+        Simulation {
+            execution_environments: vec![]
+        }
+    }
+
+    pub fn create_execution_environment(&mut self, code: Vec<u8>, state: Vec<u8>) -> usize {
+        let execution_environment = Execution_Environment {
+            code: code,
+            state: state,
+            root: Cell::new([0u8; 32])
+        };
+        self.execution_environments.push(execution_environment);
+        self.execution_environments.len()
+    }
+
+    pub fn create_shard_block(&self, index: usize, txs: Vec<Vec<u8>>) {
+        let execution_environment = &self.execution_environments[index];
+        let data :&[u8] = txs[0].as_ref();  // TODO: assuming 1 tx
+        let mut runtime = RootRuntime::new(&execution_environment.code, data, execution_environment.root.get());
+
+
+        // set new root
+        let root = runtime.execute(); 
+        execution_environment.root.set(root);
+        // set new state
+        let bytes: [u8; 4] = unsafe { transmute(1u32.to_be()) }; 
+        execution_environment.state.to_owned().append(&mut bytes.to_vec());
+    }
+
+    pub fn get_execution_environment_state(&self, index: usize) -> Vec<u8> {
+        let execution_environment = &self.execution_environments[index];
+        execution_environment.state.to_owned()
+    }
+
+    pub fn get_execution_environment_root(&self, index: usize) ->  [u8;32] {
+        let execution_environment = &self.execution_environments[index];
+        execution_environment.root.get()
+    }
+
+
+}
