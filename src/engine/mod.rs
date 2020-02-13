6fea8e48b249b71c4d19d8a70422e4e6709cd04c
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2020-02-13 17:55:43 -0600

    Engine implementation (#3)
    * stubbed out engine and primitves
    
    * fmt mods
    
    * removing primitives
    
    * adding dummy simulation implementation in order to work out engine methods
    
    * index off by one
    
    * added sep param for root
    
    * mods made to simulation to make see tests work
    
    * cargo fmt
    
    * mods suggested by linter

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,16 +1,36 @@
-pub struct Engine {}
+mod simulation;
+use simulation::Simulation;
+
+pub struct Engine {
+    simulation: Simulation,
+}
 
 impl Engine {
     pub fn new() -> Engine {
-        Engine {}
+        Engine {
+            simulation: Simulation::new(),
+        }
     }
 
     /// Deploy EE code onchain
-    pub fn deploy(&self, code: Vec<u8>, state: Vec<u8>) {}
+    pub fn deploy(&mut self, code: Vec<u8>, state: Vec<u8>, root: [u8; 32]) -> usize {
+        self.simulation
+            .create_execution_environment(code, state, root)
+    }
 
-    /// Send tx to be run onchain
-    pub fn run(&self, tx: Vec<u8>) {}
+    /// Send tx(s) to be run onchain
+    pub fn run(&self, index: usize, txs: Vec<Vec<u8>>) {
+        self.simulation.create_shard_block(index, txs);
+    }
 
     /// Get onchain state root
-    pub fn get_root(&self) {}
+    pub fn get_root(&self, index: usize) -> [u8; 32] {
+        self.simulation.get_execution_environment_root(index)
+    }
+}
+
+impl Default for Engine {
+    fn default() -> Self {
+        Self::new()
+    }
 }
---
+++ src/engine/simulation.rs
@@ -0,0 +1,75 @@
+use std::cell::Cell;
+use std::mem::transmute;
+extern crate byteorder;
+use byteorder::{LittleEndian, WriteBytesExt};
+use ewasm::{Execute, RootRuntime};
+use std::convert::TryFrom;
+
+pub struct ExecutionEnvironment {
+    code: Vec<u8>,
+    state: Vec<u8>,
+    root: Cell<[u8; 32]>,
+}
+
+pub struct Simulation {
+    execution_environments: Vec<ExecutionEnvironment>,
+}
+
+impl Simulation {
+    pub fn new() -> Simulation {
+        Simulation {
+            execution_environments: vec![],
+        }
+    }
+
+    pub fn create_execution_environment(
+        &mut self,
+        code: Vec<u8>,
+        state: Vec<u8>,
+        root: [u8; 32],
+    ) -> usize {
+        let execution_environment = ExecutionEnvironment {
+            code,
+            state,
+            root: Cell::new(root),
+        };
+        self.execution_environments.push(execution_environment);
+        self.execution_environments.len() - 1
+    }
+
+    pub fn create_shard_block(&self, index: usize, txs: Vec<Vec<u8>>) {
+        let execution_environment = &self.execution_environments[index];
+        let mut num_tx: Vec<u8> = Vec::new();
+        num_tx
+            .write_u32::<LittleEndian>(u32::try_from(txs.len()).unwrap())
+            .unwrap();
+        let data = &[
+            num_tx.to_vec(),
+            txs[0].to_owned(),
+            execution_environment.state.to_owned(),
+        ]
+        .concat();
+        println!("pre root: {:?}", execution_environment.root.get());
+        let mut runtime = RootRuntime::new(
+            &execution_environment.code,
+            data,
+            execution_environment.root.get(),
+        );
+
+        // set new root
+        let root = runtime.execute();
+        println!("post root: {:?}", root);
+        execution_environment.root.set(root);
+        // set new state
+        let bytes: [u8; 4] = unsafe { transmute(1u32.to_be()) };
+        execution_environment
+            .state
+            .to_owned()
+            .append(&mut bytes.to_vec());
+    }
+
+    pub fn get_execution_environment_root(&self, index: usize) -> [u8; 32] {
+        let execution_environment = &self.execution_environments[index];
+        execution_environment.root.get()
+    }
+}
