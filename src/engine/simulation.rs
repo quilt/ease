4137e643d7480610fd3b6c4cddf5cf98f27edee0
jonny rhea <jonathan.rhea@gmail.com>
2020-02-13 17:11:08 -0600

    mods made to simulation to make see tests work

--- src/engine/simulation.rs
+++ src/engine/simulation.rs
@@ -1,5 +1,8 @@
 use std::cell::Cell;
 use std::mem::transmute;
+extern crate byteorder;
+use byteorder::{ByteOrder, LittleEndian, WriteBytesExt};
+use std::convert::TryFrom;
 use ewasm::{Execute, RootRuntime};
 
 pub struct Execution_Environment {
@@ -34,21 +37,19 @@ impl Simulation {
 
     pub fn create_shard_block(&self, index: usize, txs: Vec<Vec<u8>>) {
         let execution_environment = &self.execution_environments[index];
-        let data :&[u8] = txs[0].as_ref();  // TODO: assuming 1 tx
+        let mut num_tx: Vec<u8> = Vec::new();
+        num_tx.write_u32::<LittleEndian>(u32::try_from(txs.len()).unwrap()).unwrap();
+        let data = &[num_tx.to_vec(),txs[0].to_owned(),execution_environment.state.to_owned()].concat();
+        println!("pre root: {:?}",execution_environment.root.get());
         let mut runtime = RootRuntime::new(&execution_environment.code, data, execution_environment.root.get());
-
 
         // set new root
         let root = runtime.execute(); 
+        println!("post root: {:?}",root);
         execution_environment.root.set(root);
         // set new state
         let bytes: [u8; 4] = unsafe { transmute(1u32.to_be()) }; 
         execution_environment.state.to_owned().append(&mut bytes.to_vec());
-    }
-
-    pub fn get_execution_environment_state(&self, index: usize) -> Vec<u8> {
-        let execution_environment = &self.execution_environments[index];
-        execution_environment.state.to_owned()
     }
 
     pub fn get_execution_environment_root(&self, index: usize) ->  [u8;32] {
